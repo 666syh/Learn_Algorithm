ca05c7c5fc6abc619fc4be19a6a293dc30418563
somebody <1317578865@qq.com>
2019-02-19 16:32:28 +0800

    from two points to middle--632ms

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -15,22 +15,43 @@ Example:
         [-1, -1, 2]
     ]
 """
-
+"""
+从两个端点到中间遍历，且跳过重复数据，减少时间复杂度
+"""
 class Solution:
     def threeSum(self, nums: 'List[int]') -> 'List[List[int]]':
-        s = []
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                a = 0-nums[i]-nums[j]
-                if a in nums[j+1:]:
-                    l = [nums[i], nums[j], a]
-                    l.sort()
-                    if l in s:
-                        continue
-                    s.append(l)
-        
-        return s
-
+        res = []
+        #先将数组排序
+        nums.sort()
+        length = len(nums)
+        for i in range(length-2):
+            #因为数组已经排好序，三个正数相加不可能等于0
+            if nums[i]>0: 
+                break 
+            #相同的i跳过
+            if i>0 and nums[i]==nums[i-1]: 
+                continue 
+            #分别从两头向中间靠拢
+            l, r = i+1, length-1 
+            while l<r:
+                total = nums[i]+nums[l]+nums[r]
+                #如果总数小了，则提高l
+                if total<0: 
+                    l+=1
+                #如果总数大了，则减小r
+                elif total>0: 
+                    r-=1
+                else: 
+                    #加入进结果集中
+                    res.append([nums[i], nums[l], nums[r]])
+                    #跳过相同元素
+                    while l<r and nums[l]==nums[l+1]: 
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]: 
+                        r-=1
+                    l+=1
+                    r-=1
+        return res
 
 x = Solution()
 print(x.threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]))
