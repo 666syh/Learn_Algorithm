e944a1fa926f75d51cb8e9a875f62f028e655f90
somebody <1317578865@qq.com>
2019-02-22 15:13:43 +0800

    devide and conquer --- 92ms

---
+++ 23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/merge-k-sorted-lists/
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+    Input:
+    [
+      1->4->5,
+      1->3->4,
+      2->6
+    ]
+    Output: 1->1->2->3->4->4->5->6
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: 'List[ListNode]') -> 'ListNode':
+        
+        def recursion(lists: 'List[ListNode]'):
+            k = len(lists)
+            if k == 0:
+                return None
+            elif k == 1:
+                return lists[0]
+            elif k == 2:
+                p, q = lists[0], lists[1]
+                l = ListNode(0)
+                h = l
+                while p!=None and q!=None:
+                    if p.val <= q.val:
+                        h.next = p
+                        p = p.next
+                    else:
+                        h.next = q
+                        q = q.next
+                    h = h.next
+                if p!=None:
+                    h.next = p
+                elif q!=None:
+                    h.next = q
+                return l.next
+            else:
+                return recursion([recursion(lists[:k//2]),recursion(lists[k//2:k])])
+        return recursion(lists)
+
