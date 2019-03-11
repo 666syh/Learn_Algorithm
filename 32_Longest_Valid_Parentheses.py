f8dfbd000fe70307bafd9637f6cd2f8e3203ae5b
somebody <1317578865@qq.com>
2019-03-11 10:22:40 +0800

    a difficult problem (dp)--56ms

--- 32_Longest_Valid_Parentheses.py
+++ 32_Longest_Valid_Parentheses.py
@@ -15,29 +15,36 @@ Example 2:
     Explanation: The longest valid parentheses substring is "()()"
 """
 '''
-stack
-zjlvmiao from https://leetcode.com/problems/longest-valid-parentheses/discuss/14141/Pure-1D-DP-without-using-stack-(python)-with-detailed-explanation 
+1d-dp
+pennlio from https://leetcode.com/problems/longest-valid-parentheses/discuss/14141/Pure-1D-DP-without-using-stack-(python)-with-detailed-explanation 
 '''
 class Solution(object):
     def longestValidParentheses(self, s):
-        if not s or len(s) < 2: return 0
-        stack = []
-        leftmost = -1           #用以记录左边最后一个无用的右括号的位置
-        res = 0
-        for i in range(len(s)):
-            if s[i] == '(':
-                stack.append(i)
-            else: # case ')'
-                if not stack: # 如果栈为空，即为无用的右括号
-                    leftmost = i
-                else:
-                    stack.pop() # 删除前方一个左括号的位置
-                    if not stack: # 如果栈为空，取当前位置与最后一个无用右括号的距离
-                        res = max(res, i - leftmost) 
-                    else: # 如果栈不为空，取当前位置与之前第二个左括号的距离
-                        a = stack[-1]
-                        res = max(res, i - stack[-1]) 
-        return res
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 一维dp
+        # dp[i] records the longestValidParenthese EXACTLY ENDING at s[i]
+        dp = [0 for x in range(len(s))]
+        max_to_now = 0
+        for i in range(1,len(s)):
+            if s[i] == ')':
+                # case 1: ()()
+                if s[i-1] == '(':
+                    # 上一次的结果+2
+                    dp[i] = dp[i-2] + 2
+                # case 2: (()) 
+                # i-dp[i-1]-1 是上一个对应于该 ")" 的 "(" 的索引
+                elif i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == '(':
+                    if dp[i-1] > 0: # 前边的右括号已经匹配了 
+                    # 之前的匹配数+2，且加上之前可能出现的第一种情况
+                        dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+                    else:
+                    # 前边的右括号没有匹配
+                        dp[i] = 0
+                max_to_now = max(max_to_now, dp[i])
+        return max_to_now
 
 
 x = Solution()
